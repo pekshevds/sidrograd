86f19218b8afb3a7a6c6b5f97ebb9ee14c54db85
Pekshev.Denis <Pekshev.Denis@sblogistica.ru>
2023-12-13 15:26:41 +0300

    add api-token-auth/ route into api_app urls

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 
 app_name = 'api_app'
@@ -9,4 +10,5 @@ urlpatterns = [
     path('wish/', include('wish_list_app.urls', namespace='wish_list_app')),
     path('cart/', include('cart_app.urls', namespace='cart_app')),
     path('order/', include('order_app.urls', namespace='order_app')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
