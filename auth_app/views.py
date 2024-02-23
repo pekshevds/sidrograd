2095cd487e4b6b0709329d1fe4afbc831d9a9499
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-02-23 06:58:08 +0300

    routes for auth_app

--- auth_app/views.py
+++ auth_app/views.py
@@ -3,6 +3,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from auth_app.models import User
 from auth_app.serializers import UserSerializer
+from auth_app.transport import send_pin_code, fetch_recipient
+from auth_app.services import (
+    add_pin,
+    authenticate,
+    update_or_create_user_token,
+    use_pin_code
+)
 
 
 class UserView(APIView):
@@ -10,7 +17,7 @@ class UserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        username = request.GET.get("username", "")
+        username = request.GET.get("username", None)
         if username:
             queryset = User.objects.filter(username=username)
             serializer = UserSerializer(queryset, many=True)
@@ -19,3 +26,53 @@ class UserView(APIView):
             serializer = UserSerializer(queryset, many=True)
         response = {"data": serializer.data}
         return Response(response)
+
+
+class UserInfoView(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer([request.user], many=True)
+        response = {"data": serializer.data}
+        return Response(response)
+
+
+class PinView(APIView):
+    """Отправляет pin для существующего пользователя.
+    Пользователь определяется по имени (номеру телефона)
+     или адресу электронной почты.
+    Порядок получения пользователя определяется в настройках.
+    """
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        recipient = request.GET.get("recipient")
+        if recipient:
+            user = fetch_recipient(recipient)
+            if user:
+                pin = add_pin(user)
+                send_pin_code(pin.pin_code, recipient)
+        return Response({"data": None})
+
+
+class TokenView(APIView):
+    """Возвращает токен по имени пользователя и актуальному пину.
+    Пользователь определяется по имени (номеру телефона)
+     или адресу электронной почты.
+    Порядок получения пользователя определяется в настройках.
+    """
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.POST.get("username")
+        pincode = request.POST.get("pincode")
+        user = authenticate(username, pincode)
+        if user is not None:
+            token = update_or_create_user_token(user=user)
+            if token is not None:
+                use_pin_code(pincode)
+                return Response({"data": {
+                    "token": token.key
+                }})
+        return Response({"data": None})
