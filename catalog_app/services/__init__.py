41aa63d9f6adf3dd663f992836b8d88f3a3c1c42
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-06-28 07:29:00 +0300

    refactoring

--- catalog_app/services/__init__.py
+++ catalog_app/services/__init__.py
@@ -2,16 +2,19 @@ from typing import List
 from django.db.models import QuerySet, Model
 from django.db import transaction
 
+# Category, Country, Filtering and etc
+CatalogModel = type[Model]
 
-def object_by_id(_class, id: str) -> Model:
+
+def object_by_id(_class: CatalogModel, id: str) -> object | None:
     return _class.objects.filter(id=id).first()
 
 
-def object_by_id_list(_class, ids: List[str]) -> QuerySet:
+def object_by_id_list(_class: CatalogModel, ids: List[str]) -> QuerySet:
     return _class.objects.filter(id__in=ids)
 
 
-def handle_object(_class, object_dir: dict) -> Model:
+def handle_object(_class: CatalogModel, object_dir: dict) -> object | None:
     key_list = ["name", "comment"]
     obj, created = _class.objects.get_or_create(id=object_dir.get("id"))
     for key in key_list:
@@ -21,7 +24,7 @@ def handle_object(_class, object_dir: dict) -> Model:
     return obj
 
 
-def handle_object_list(_class, object_list: List[dict]) -> QuerySet:
+def handle_object_list(_class: CatalogModel, object_list: List[dict]) -> QuerySet:
     ids = []
     with transaction.atomic():
         for object_item in object_list:
