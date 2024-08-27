a7ecd902b33a1a0d2cbb9e8de9e559e67eb90865
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-08-27 12:31:46 +0300

    put callbackview allow any

--- api_app/views.py
+++ api_app/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpRequest
 from rest_framework.views import APIView
-from rest_framework import permissions, authentication
+from rest_framework import permissions  # , authentication
 from rest_framework.response import Response
 from api_app.serializers import CallBackSerializer
 from api_app.services import process_call_back_data
 
 
 class CallBackView(APIView):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request: HttpRequest) -> Response:
         serializer = CallBackSerializer(data=request.data)
